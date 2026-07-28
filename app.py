f98171dc99a0b29c74c9ce94b16360b4898b60c2
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-28 20:24:15 +0500

    added all the inputs

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ import pandas as pd
 
 model = joblib.load("spaceship_titanic_pipeline.joblib")
 
+PassengerId = st.text_input("Enter the id: ")
+HomePlanet = st.text_input("HomePlanet: ")
+CryoSleep = st.selectbox([True , False])
+Cabin = st.text_input("Enter the cabin: ")
+Destination = st.text_input("destination: ")
+Age = st.number_input("age")
+VIP = st.selectbox([True , False])
+RoomService = st.number_input("paid: ")
+FoodCourt = st.number_input("paid: ")
+ShoppingMall = st.number_input("paid: ")
+Spa = st.number_input("paid: ")
+VRDeck = st.number_input("paid: ")
+Name = st.text_input("name: ")
